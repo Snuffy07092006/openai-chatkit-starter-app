4d875f5bfbebfad638518f7a3deca844c8e80ab5
Snuffy07092006 <daniel.kjaer.rasmussen@gmail.com>
2025-10-18 21:56:03 +0200

    Update config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -16,7 +16,7 @@ export const PLACEHOLDER_INPUT = "Ask anything...";
 
 export const GREETING = "How can I help you today?";
 
-export const getThemeConfig = (theme: ColorScheme: 'Dark'): ThemeOption => ({
+export const getThemeConfig = (theme: ColorScheme): ThemeOption => ({
   color: {
     grayscale: {
       hue: 120,
