15b1bd53e488445d8bd5cbf7f962c7da20aca880
Snuffy07092006 <daniel.kjaer.rasmussen@gmail.com>
2025-10-18 20:40:21 +0200

    Update config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -1,35 +1,47 @@
-import { ColorScheme, StartScreenPrompt, ThemeOption } from "@openai/chatkit";
+import type { ChatKitOptions } from "@openai/chatkit";
 
-export const WORKFLOW_ID =
-  process.env.NEXT_PUBLIC_CHATKIT_WORKFLOW_ID?.trim() ?? "";
-
-export const CREATE_SESSION_ENDPOINT = "/api/create-session";
-
-export const STARTER_PROMPTS: StartScreenPrompt[] = [
-  {
-    label: "What can you do?",
-    prompt: "What can you do?",
-    icon: "circle-question",
+const options: ChatKitOptions = {
+  api: {
+    // TODO: configure your ChatKit API integration (URL, auth, uploads).
   },
-];
-
-export const PLACEHOLDER_INPUT = "Ask anything...";
-
-export const GREETING = "How can I help you today?";
-
-export const getThemeConfig = (theme: ColorScheme): ThemeOption => ({
-  color: {
-    grayscale: {
-      hue: 220,
-      tint: 6,
-      shade: theme === "dark" ? -1 : -4,
+  theme: {
+    colorScheme: 'dark',
+    radius: 'round',
+    density: 'compact',
+    color: {
+      accent: {
+        primary: '#303030',
+        level: 1
+      },
+      surface: {
+        background: '#0f3008',
+        foreground: '#303030'
+      }
     },
-    accent: {
-      primary: theme === "dark" ? "#f1f5f9" : "#0f172a",
-      level: 1,
+    typography: {
+      baseSize: 14,
+      fontFamily: 'Lora, serif',
+      fontSources: [
+        {
+          family: 'Lora',
+          src: 'https://fonts.gstatic.com/s/lora/v37/0QIvMX1D_JOuMwr7I_FMl_E.woff2',
+          weight: 400,
+          style: 'normal',
+          display: 'swap'
+        }
+      // ...and 7 more font sources
+      ]
+    }
+  },
+  composer: {
+    placeholder: 'Stil et spørgsmål',
+    attachments: {
+      enabled: false
     },
   },
-  radius: "round",
-  // Add other theme options here
-  // chatkit.studio/playground to explore config options
-});
+  startScreen: {
+    greeting: 'Hvad kan jeg hjælpe med idag?',
+    prompts: [],
+  },
+  // Optional fields not shown: locale, initialThread, threadItemActions, header, onClientTool, entities, widgets
+};
