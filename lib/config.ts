5fca8a48d96b9d9f7f259e176591282469e507e9
Snuffy07092006 <daniel.kjaer.rasmussen@gmail.com>
2025-10-18 22:05:26 +0200

    Update config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -29,9 +29,6 @@ export const getThemeConfig = (theme: ColorScheme): ThemeOption => ({
     },
   },
   radius: "round",
-  typography: {
-    baseSize: 14,
-    fontFamily: 'Lora, serif',
   // Add other theme options here
   // chatkit.studio/playground to explore config options
 });
